aa84d6192eb83dfc72b8c89f56c6226561ca7967
diszell2008 <listening2009@gmail.com>
2016-01-24 15:17:02 +0700

    Add Camera Parameters
    Change-Id: I83bb1a898850804df4aeb3b2823087249ded5e3b

--- ef63s.mk
+++ ef63s.mk
@@ -66,10 +66,6 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.ef63s
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
